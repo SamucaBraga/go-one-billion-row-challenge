cfcd8db3d8732acebf1e492ea478192ffdc79f9c
SamucaBraga <samuelbgf@hotmail.com>
2024-11-07 09:48:58 -0300

    feat: finish challenge

--- m.go
+++ m.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 type Measurements struct {
-	Nome  string
 	Min   float64
 	Max   float64
 	Sum   float64
@@ -23,13 +25,53 @@ func main() {
 
 	defer measurements.Close()
 
+	dados := make(map[string]Measurements)
+
 	scanner := bufio.NewScanner(measurements)
 
 	for scanner.Scan() {
 		rawData := scanner.Text()
 		semicolon := strings.Index(rawData, ";")
 		location := rawData[:semicolon]
-		temp := rawData[semicolon+1:]
+		rawTemp := rawData[semicolon+1:]
+
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurements, ok := dados[location]
+		if !ok {
+			measurements = Measurements{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurements.Min = min(measurements.Min, temp)
+			measurements.Min = max(measurements.Max, temp)
+			measurements.Sum += temp
+			measurements.Count++
+		}
+
+		dados[location] = measurements
 	}
 
+	locations := make([]string, 0, len(dados))
+	for name := range dados {
+		locations = append(locations, name)
+	}
+
+	sort.Strings(locations)
+
+	fmt.Printf("{")
+	for _, name := range locations {
+		measurements := dados[name]
+		fmt.Printf(
+			"%s=%.1f/%.1f/%.1f, ",
+			name,
+			measurements.Min,
+			measurements.Sum/float64(measurements.Count),
+			measurements.Max,
+		)
+	}
+	fmt.Printf("}")
 }
